7482ba936456f3551c2b047ad91b81b1098463c0
ThopsD <74445064+thops1925@users.noreply.github.com>
2021-09-10 23:48:22 +0800

    add env

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ const mongoose = require('mongoose');
 const path = require('path');
 const photo = require('./routers/FileUploadRoutes');
 const app = express();
-mongoose.connect(
-  'mongodb+srv://may21987:may21987@cluster0.0n05x.mongodb.net/gallery?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-);
+mongoose.connect(process.env.API, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 mongoose.connection.once('open', () => {
   console.log('open connection');
 });
@@ -20,4 +17,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/upload', express.static(path.join(__dirname, 'upload')));
 app.use('/api', photo.routes);
-app.listen(5000, () => console.log('connected'));
+app.listen(5000 || process.env.PORT, () => console.log('connected'));
