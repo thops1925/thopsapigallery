5f204b486903a549ff34c50d18af63016d725350
ThopsD <74445064+thops1925@users.noreply.github.com>
2021-09-10 21:37:57 +0800

    delete .env

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 const photo = require('./routers/FileUploadRoutes');
 const app = express();
-mongoose.connect(process.env.API, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://may21987:may21987@cluster0.0n05x.mongodb.net/gallery?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 mongoose.connection.once('open', () => {
   console.log('open connection');
 });
